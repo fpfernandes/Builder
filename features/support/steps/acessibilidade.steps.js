8187327835409d01afbacaa77184f39688b9d221
Fabiana Fernandes <128615902+fpfernandes@users.noreply.github.com>
2026-05-09 17:41:23 +0100

    Change login credentials to empty strings
    Updated login credentials to empty strings for testing.

--- features/support/steps/acessibilidade.steps.js
+++ features/support/steps/acessibilidade.steps.js
@@ -17,7 +17,7 @@ Given('que estou acessando o {string} do {string} gerado pela Builder', async fu
   await this.page.goto("https://builderstudio-qa-prod.smartlms.com.br/")
 
   await this.loginPage.titulo_pagina()
-  await this.loginPage.login('usuario.iterasys', 'Iterasys@123')
+  await this.loginPage.login('', '')
   await this.loginPage.botao_login()
 
   await this.page.waitForLoadState('networkidle')
